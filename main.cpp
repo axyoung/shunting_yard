1cc007628c4e68cb61f3cf86fd67cc553dc1bab8
cixay <alex.shan.young@gmail.com>
2018-02-08 12:21:41 -0800

    first

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * Alex Young
+ * Shunting Yard
+ * C++ and data structures (Galbraith)
+ * This program will read a mathematical expression entered with infix notation, using spaces between each token. It will then output the postfix expression. You should then be able to output infix, prefix, or postfix notation.
+ * It will do this using a stack which will be used to create expression trees
+ * Last updated 2/8/2018
+ */
+
+using namespace std;
+
+int main () {
+
+// if there on the same level then pop the operation out of the stack in to the output, unless it is ^:
+
+	return 0;
+}
+
+// yay I finished
